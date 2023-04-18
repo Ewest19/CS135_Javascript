86c34aeba19d05b8cb45a36d7f4f74496190406c
Ewest19 <Ewest19@mail.wou.edu>
2023-04-17 22:44:17 -0700

    Added a second page with some filler text and some styling

--- src/KitchenBuddy/src/routes/+page.svelte
+++ src/KitchenBuddy/src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <html>
+    <head>
+        <title>Welcome to Kitchen Buddy</title>
+    </head>
+    <body>
+        <h1>Welcome to Kitchen Buddy!</h1>
+
+        <br />
+        <p> The <a href="/GroceryList">Grocery List</a></p>    
+    
+    </body>
+</html> -->
+
+
+<section>
+    <h1>Welcome to Kitchen Buddy!</h1>
+
+    <br />
+    <p> The <a href="/GroceryList">Grocery List</a></p>    
+</section>
+
+
+<style>
+    section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+        background-color:lavender;
+	}
+
+    :global(body) {
+        background-color: lavender;
+        transition: background-color 0.3s
+    }
+
+    :global(text) {
+        color: rgb(21, 21, 23);
+    }
+
+    h1 {
+        width: 100%;
+        text-align: center;
+        color: rgb(21, 21, 23);
+    }
+
+    a {
+        font-size: large;
+    }
+
+
+
+</style>
---
+++ src/KitchenBuddy/src/routes/GroceryList/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Grocery List</title>
+	<meta name="description" content="Grocery List" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Grocery List</h1>
+	<p>Welcome to your grocery list home page</p>
+</div>
